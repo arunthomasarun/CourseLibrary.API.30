c3753227910fd7bf2914f0f2f5611d8fa2eedeb2
Arun Thomas <ArunThomas04@gmail.com>
2020-06-08 05:39:52 +0530

    Add project files.

---
+++ CourseLibrary.API.30/Startup.cs
@@ -0,0 +1,54 @@
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CourseLibrary.API._30
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           services.AddControllers();
+             
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=DESKTOP-C8CNSOE\\SQLEXPRESS;Database=CourseLibrary.API.30;user id=sa;password=SqlExpress@9;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
